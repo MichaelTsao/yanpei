94197377fea45d30b8af59e17279d806d04c7cb6
Michael Tsao <caoxiang@yeah.net>
2016-11-09 13:58:25 +0800

    new layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,21 +1,79 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\assets\AppAsset;
+use yii\helpers\Html;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
 <!doctype html>
-<html lang="en">
+<html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
     <title>e听</title>
-    <link rel="stylesheet" href="/res/css/style.css"/>
-    <script src="/js/public/jquery.js"></script>
-    <script src="/js/public/js.cookie.js"></script>
-    <script type="text/javascript" src="/js/public/jquery.touchSlider.js"></script>
-    <script src="/js/public/placehoders.js"></script>
-    <script src="/js/public/slide.js"></script>
-    <script src="/js/public/slide_use.js"></script>
-    <link rel="stylesheet" href="/js/new/laydate/need/laydate.css"/>
-    <link rel="stylesheet" href="/css/lightbox.css"/>
-    <script src="/js/new/laydate/laydate.js"></script>
-    <script src="/js/public/effect.js"></script>
+    <?php $this->head() ?>
+
+<!--    <link rel="stylesheet" href="/res/css/style.css"/>-->
+<!--    <script src="/js/public/jquery.js"></script>-->
+<!--    <script src="/js/public/js.cookie.js"></script>-->
+<!--    <script type="text/javascript" src="/js/public/jquery.touchSlider.js"></script>-->
+<!--    <script src="/js/public/placehoders.js"></script>-->
+<!--    <script src="/js/public/slide.js"></script>-->
+<!--    <script src="/js/public/slide_use.js"></script>-->
+<!--    <link rel="stylesheet" href="/js/new/laydate/need/laydate.css"/>-->
+<!--    <link rel="stylesheet" href="/css/lightbox.css"/>-->
+<!--    <script src="/js/new/laydate/laydate.js"></script>-->
+<!--    <script src="/js/public/effect.js"></script>-->
+
 </head>
 <body style="background: #f4f4f6">
+<?php $this->beginBody() ?>
+
+<div class="navbar-wrapper">
+    <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Project name</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </div>
+</div>
+
+
 <div class="H-nav clearfix">
     <div class="H-nav-con clearfix">
         <div class="H-nav-con-left clearfix">
@@ -124,5 +182,7 @@
 <script src="/js/public/effect.js"></script>
 <script src="/js/lightbox.js"></script>
 
+<?php $this->endBody() ?>
 </body>
 </html>
+<?php $this->endPage() ?>
